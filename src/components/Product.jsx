57cd9849d56a7ce7cf0b0d402fb8cd6de7f65563
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2024-03-11 00:46:52 +0200

    meh

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,22 @@
 // src/Product.jsx
 
-const Product = () => {
-      return (
-        <div>
-          <h2>Tacos</h2>
-                <p>Price: 999 credits</p>
-        </div>
-      );
-    };
+export const Product = () => {
+	const price = 999;
+	const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
+
+  return (
+    <>
+      <h2>Tacos</h2>
+			<img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </>
+  );
+};
     
     export default Product;
     
+    <label htmlFor="login">
+  <input id="login" />
+    </label>
+
+    // остановился на Властивості компонента (props)
