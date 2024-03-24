c3cffa893c506f082f9271ec13ba3546ac1bc842
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2024-03-24 12:39:27 +0200

    good2

---
+++ src/components/transaction.jsx
@@ -0,0 +1,25 @@
+import css from 'src/components/transactionStyle.css';
+
+export default function transaction({ items }) {
+  return (
+    <table className={css.table}>
+      <thead>
+        <tr>
+          <th className={css.tableHeadCell}>Type</th>
+          <th className={css.tableHeadCell}>Amount</th>
+          <th className={css.tableHeadCell}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={css.tableRow} key={id}>
+            <td className={css.tableBodyCell}>{type}</td>
+            <td className={css.tableBodyCell}>{amount}</td>
+            <td className={css.tableBodyCell}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
